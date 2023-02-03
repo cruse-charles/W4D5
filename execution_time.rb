1b26870219c2dd63c2768adee2f82afd70ba431a
Charles Cruse <charle9@vt.edu>
2023-02-03 13:54:26 -0500

    finished my min

--- execution_time.rb
+++ execution_time.rb
@@ -1,1 +1,32 @@
-aaa
+def my_min(list)
+    min = list.min
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list) 
+
+#O(n) time complexity
+
+
+
+def my_min2(list)
+    min = list.first
+    (0...list.length).each do |idx|
+        if min > list[idx]
+            min = list[idx]
+        end
+    end
+
+    min
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min2(list) 
+
+
+#O(n) time complexity
+
+
+def largest_contiguous_subsum(list)
+    
+end
