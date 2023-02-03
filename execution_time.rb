3258b50bae6f83e22f8f737c0d7cd9b6f3282406
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-03 14:11:02 -0500

    Largest_contigous_subsum_2

--- execution_time.rb
+++ execution_time.rb
@@ -28,5 +28,42 @@ p my_min2(list)
 
 
 def largest_contiguous_subsum(list)
-    
+    arr = []
+    sums = []
+
+    (0...list.length).each do |i|
+        (i...list.length).each do |j|
+            # if j>i 
+                arr << list[i..j]
+            # end
+        end
+    end
+    arr.each do |sub_arr|
+        sums << sub_arr.sum 
+    end
+    sums.max
 end
+list = [5, 3, -7]
+p largest_contiguous_subsum(list)
+
+
+def largest_contiguous_subsum_2(list)
+    current_sum = 0
+    max_sum = 0
+    
+    
+    (0...list.length-1).each do |i|
+        if list[i+1] > 0
+            current_sum += list[i]
+        else
+            if current_sum > max_sum
+                max_sum = current_sum
+                current_sum = 0
+            end
+        end
+    end
+    max_sum
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum_2(list)
